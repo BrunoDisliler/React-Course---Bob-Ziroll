7515c2bad2c3e31306b9345dc8f3fe09adeffec9
BrunoDisliler <bruno_disliler@outlook.com>
2022-03-15 10:27:50 -0300

    Fix

--- digital-business-card/business-card/src/Components/Footer.jsx
+++ digital-business-card/business-card/src/Components/Footer.jsx
@@ -3,13 +3,13 @@ import './styles/footer.css'
 import facebook from '../icons/Facebook Icon.png'
 import instagram from '../icons/Instagram Icon.png'
 import github from '../icons/GitHub Icon .png'
-import twitter from '../icons/Twitter Icon.png'
+import twitter from '../icons/Linkedin Icon.png'
 
 export default class Footer extends React.Component {
   render() {
     return (
-      <div id='social'>
-        <img src={twitter} alt="twitter" width='25px' id='twitter'/>
+      <div id="social">
+        <img src={twitter} alt="linkedin" width='25px' id='twitter'/>
         <img src={facebook} alt="facebook" width='25px' id='facebook'/>
         <img src={instagram} alt="instagram" width='25px' id='instagram' />
         <img src={github} alt="github" width='25px' id='github'/>
