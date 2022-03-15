077b55e021233af281ce4ac96724c8c9bab65d4c
BrunoDisliler <bruno_disliler@outlook.com>
2022-03-15 16:41:49 -0300

    Final commit business card

--- digital-business-card/business-card/src/Components/Footer.jsx
+++ digital-business-card/business-card/src/Components/Footer.jsx
@@ -3,16 +3,27 @@ import './styles/footer.css'
 import facebook from '../icons/Facebook Icon.png'
 import instagram from '../icons/Instagram Icon.png'
 import github from '../icons/GitHub Icon .png'
-import twitter from '../icons/Linkedin Icon.png'
+import Linkedin from '../icons/Linkedin Icon.png'
 
 export default class Footer extends React.Component {
   render() {
     return (
       <div id="social">
-        <img src={twitter} alt="linkedin" width='25px' id='twitter'/>
-        <img src={facebook} alt="facebook" width='25px' id='facebook'/>
-        <img src={instagram} alt="instagram" width='25px' id='instagram' />
-        <img src={github} alt="github" width='25px' id='github'/>
+        <a href="https://www.linkedin.com/in/brunodisliler/" target="__blank">
+          <img src={Linkedin} alt="linkedin" width='40px' id='twitter'/>
+        </a>
+
+        <a href="https://pt-br.facebook.com/bruno.desouzadisliler" target="__blank">
+          <img src={facebook} alt="facebook" width='40px' id='facebook'/>
+        </a>
+
+        <a href="https://www.instagram.com/brunodisliler/" target="__blank">
+          <img src={instagram} alt="instagram" width='40px' id='instagram' />
+        </a>
+
+        <a href="https://github.com/BrunoDisliler" target="__blank">
+          <img src={github} alt="github" width='40px' id='github'/>
+        </a>
       </div>
     );
   }
--- digital-business-card/business-card/src/Components/Info.jsx
+++ digital-business-card/business-card/src/Components/Info.jsx
@@ -10,8 +10,12 @@ export default class Info extends React.Component {
         <h2 id='name'>Bruno Disliler</h2>
         <h3 id='cargo'>Frontend Developer</h3>
         <h5 id='email'>bruno_disliler@outlook.com</h5>
-        <button type='button' id='btn-email'>Email</button>
-        <button type='button' id='btn-linkedin'>Linkedin</button>
+        <a href="mailto:bruno_disliler@outlook.com" target="__blank">
+          <button type="button" id='btn-email'>Email</button>
+        </a>
+        <a href="https://www.linkedin.com/in/brunodisliler/" target="__blank">
+          <button type='button' id='btn-linkedin'>Linkedin</button>
+        </a>
       </div>
     );
   }
